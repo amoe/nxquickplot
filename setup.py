02761cd34c184ade3b4400cb13e512e414538136
David Banks <amoebae@gmail.com>
2019-08-06 09:34:30 +0100

    new apis, documentation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nxquickplot",
-    version="0.0.3",
+    version="1.0.0",
     author="David Banks",
     author_email="amoebae@gmail.com",
     description="Convenience package for drawing NetworkX graphs",
